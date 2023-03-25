f2d02afd8fab7354154d6986e5c583c6047b85d2
dmitriy152 <romanovds33@yandex.ru>
2023-03-25 10:37:12 +0300

    Add your commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let buttonPopupOpen = document.querySelector(".menu__button")
 let popupMenu = document.querySelector(".header__popup")
 function popupHeader (){
-    buttonPopupOpen.addEventListener("click", function(){
+    buttonPopupOpen.addEventListener("click", () => {
         if (buttonPopupOpen.classList.contains("menu__button-active")){
             buttonPopupOpen.classList.remove("menu__button-active")
             popupMenu.classList.remove("header__popup-active")
@@ -18,12 +18,12 @@ let modalCatalog = document.querySelector(".modal__catalog")
 let closeCatalogModal = document.querySelector(".close_catalog_btn")
 let circleAdvantages = document.querySelector(".circle__advertanges")
 let circleCover = document.querySelector(".circle__cover")
-setTimeout(function(){
+setTimeout(() => {
     modalCatalog.classList.add("modal__catalog-active")
     circleAdvantages.classList.add("circle__advertanges-active")
     circleCover.classList.add("circle__cover-active")
     if(modalCatalog.classList.contains("modal__catalog-active")){
-        setTimeout(function(){
+        setTimeout(() => {
             modalCatalog.classList.remove("modal__catalog-active")
             circleAdvantages.classList.remove("circle__advertanges-active")
             circleCover.classList.remove("circle__cover-active")
@@ -31,7 +31,7 @@ setTimeout(function(){
     }
 }, 2500)
 
-closeCatalogModal.addEventListener("click", function(){
+closeCatalogModal.addEventListener("click", () => {
     modalCatalog.classList.remove("modal__catalog-active")
     circleAdvantages.classList.remove("circle__advertanges-active")
     circleCover.classList.remove("circle__cover-active")
@@ -49,13 +49,13 @@ let openConsultation = document.querySelectorAll(".open__consultation")
 let consultationInputName = document.querySelector(".input__name")
 let consultationInputPhone = document.querySelector(".input__tel")
 for(let i = 0; i < openConsultation.length; i++){
-    openConsultation[i].addEventListener("click", function(){
+    openConsultation[i].addEventListener("click", () => {
         overlay.classList.add("overlay-active")
         modalConsultation.classList.add("modal__window__consultation-active")
     })
 }
 
-closeConsultationWindow = function (e){
+closeConsultationWindow = (e) => {
     e.preventDefault()
     overlay.classList.remove("overlay-active")
     modalConsultation.classList.remove("modal__window__consultation-active")
@@ -71,11 +71,19 @@ if (window.matchMedia('screen and (min-width:728px) and (max-width:1439px)').mat
     journalTitel.innerHTML = "Получите каталог <span class=\"bold\">с самыми популярными </span><span class=\"bold\">объектами</span> недвижимости</span> в Дубае";
 }
 let journal = document.querySelector(".journal")
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     if(window.scrollY > journal.offsetTop - 300){
         journal.classList.add("journal-animation")
     }
 })
+
+let downoalCatalog = document.querySelectorAll(".catalog__download")
+for (let i = 0; i < downoalCatalog.length; i++){
+    downoalCatalog[i].addEventListener("click", (e)=>{
+        e.preventDefault()
+        window.open('catalog.pdf')
+    })
+}
 
 
 var swiper = new Swiper(".mySwiper", {
@@ -94,26 +102,26 @@ var swiper = new Swiper(".mySwiper", {
   let imageMax = document.querySelector(".image__max")
   let cards = swiper1.querySelectorAll(".card__photo")
   for(let i = 0; i < cards.length; i++){
-    cards[i].addEventListener("click", function(){
-        url = this.src
+    cards[i].addEventListener("click", () => {
+        url = cards[i].src
         if(imageMax.src == url){
             imageMax.classList.remove("image__max-active")
             imageMax.src = ""
         }
         else {
-            imageMax.src=url
+            imageMax.src = url
             imageMax.classList.add("image__max-active")
         }
-        swiper1.querySelector(".swiper-button-next").addEventListener("click", function(){
+        swiper1.querySelector(".swiper-button-next").addEventListener("click", () => {
             imageMax.classList.remove("image__max-active")
             imageMax.src = ""
         })
-        swiper1.querySelector(".swiper-button-prev").addEventListener("click", function(){
+        swiper1.querySelector(".swiper-button-prev").addEventListener("click", () => {
             imageMax.classList.remove("image__max-active")
             imageMax.src = ""
         })
     })
-    imageMax.addEventListener("click", function(){
+    imageMax.addEventListener("click", () => {
         imageMax.src=""
         imageMax.classList.remove("image__max-active")
     })
@@ -141,7 +149,7 @@ var swiper = new Swiper(".mySwiper", {
   let buttonMore = swiper1.querySelectorAll(".price__more")
   
   for(let i = 0; i < buttonMore.length; i++){
-    buttonMore[i].addEventListener("click", function(){
+    buttonMore[i].addEventListener("click", () => {
         imageMax.classList.remove("image__max-active")
         imageMax.src = ""
         modalMore.classList.add("modal__card__more-active")
@@ -165,7 +173,7 @@ var swiper = new Swiper(".mySwiper", {
     })
   }
     for(let k = 0; k < modalMoreMinImage.length; k++){
-        modalMoreMinImage[k].addEventListener("click", function(){
+        modalMoreMinImage[k].addEventListener("click", () => {
             modalMoreMainImage.src = modalMoreMinImage[k].src
         })
     }
@@ -175,13 +183,13 @@ var swiper = new Swiper(".mySwiper", {
     imageMax.src = ""
     modalMore.classList.remove("modal__card__more-active")
   }
-  closeModalMore.addEventListener("click", function(){
+  closeModalMore.addEventListener("click", () => {
     closeModalCatalog()
   })
-  swiper1.querySelector(".swiper-button-prev").addEventListener("click", function(){ 
+  swiper1.querySelector(".swiper-button-prev").addEventListener("click", () => { 
     closeModalCatalog()
   })
-  swiper1.querySelector(".swiper-button-next").addEventListener("click", function(){ 
+  swiper1.querySelector(".swiper-button-next").addEventListener("click", () => { 
     closeModalCatalog()
   })
   var swiper2 = new Swiper(".mySwiper2", {
@@ -193,13 +201,13 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-  document.querySelector(".slide__main__next").addEventListener("click",function(){
+  document.querySelector(".slide__main__next").addEventListener("click",() => {
     swiper2.slideNext()
   })
 
   let nextBtn = document.querySelectorAll(".progress__button__next")
   for(let i = 0; i < nextBtn.length; i++){
-    nextBtn[i].addEventListener("click", function(){
+    nextBtn[i].addEventListener("click", () => {
         let activeSlide = document.querySelector(".mySwiper2").querySelector(".swiper-slide-active")
         let checkboxCounter = activeSlide.querySelectorAll('input[type="radio"]:checked').length
         console.log(checkboxCounter)
@@ -213,7 +221,50 @@ var swiper = new Swiper(".mySwiper", {
   }
   let prevtBtn = document.querySelectorAll(".progress__button__prev")
   for(let i = 0; i < prevtBtn.length; i++){
-    prevtBtn[i].addEventListener("click", function(){
+    prevtBtn[i].addEventListener("click", () => {
         swiper2.slidePrev()
     })
   }
+  let headerHeight = document.querySelector(".header").offsetHeight
+  let navItem = document.querySelectorAll(".navigation__item")
+  for (let i = 0; i < navItem.length; i++){
+    navItem[i].addEventListener("click", (e) => {
+        e.preventDefault()
+        if (navItem[i].classList.contains("item__main")){
+            let scrollElem =  document.querySelector(".cover").offsetTop - headerHeight
+            window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+        }
+        else if (navItem[i].classList.contains("item__partners")){
+            let scrollElem =  document.querySelector(".partners").offsetTop - headerHeight
+            window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+        }
+        else if (navItem[i].classList.contains("item__catalog")){
+            let scrollElem =  document.querySelector(".catalog__slider").offsetTop - headerHeight
+            window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+        }
+        else if (navItem[i].classList.contains("item__cryptocurrency")){
+            let scrollElem =  document.querySelector(".cryptocurrency").offsetTop - headerHeight
+            window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+        }
+        else if (navItem[i].classList.contains("item__journal")){
+            let scrollElem =  document.querySelector(".journal").offsetTop - headerHeight
+            window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+        }
+        else if (navItem[i].classList.contains("item__telegram")){
+            let scrollElem =  document.querySelector(".telegram__cover").offsetTop - headerHeight
+            window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+        }
+    })
+  }
+
+  let fastSearchProject = document.querySelector(".search__project")
+  fastSearchProject.addEventListener("click", () => {
+    let scrollElem =  document.querySelector(".survey").offsetTop - headerHeight
+    window.scrollTo({ top: scrollElem, behavior: 'smooth'});
+  })
+
+  let privacyPolicy = document.querySelector(".footer__privacy")
+  privacyPolicy.addEventListener("click", (e) => {
+    e.preventDefault()
+    window.open('privacy_policy.rtf')
+  })
